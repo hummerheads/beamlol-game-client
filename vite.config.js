3fbedd1bd57b04211c94faa2eac64c7000bd7226
Abdullah Al Mamun <101423827+hummerheads@users.noreply.github.com>
2024-11-04 15:53:28 +0600

    DB DOne

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      "/api": "https://astounding-licorice-1ef290.netlify.app/"
+    }
+  },
   plugins: [react()],
 })
